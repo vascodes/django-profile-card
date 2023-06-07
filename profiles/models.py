d25514355ef3fcdcb9dd2609596f935a334654ea
Nikhil Vas <mca2240@rajagiri.edu>
2023-06-07 12:03:56 +0530

    Create profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=10)
+    email = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    github = models.CharField(max_length=2550)
+    qualification = models.CharField(max_length=255)
+    tech_skills = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    about = models.CharField(max_length=255)
+    interests = models.CharField(max_length=255)
+    image = models.CharField(max_length=2550)
