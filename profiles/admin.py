ce0210f84897cb7f710cd75eef3ce814961f7d4d
Nikhil Vas <mca2240@rajagiri.edu>
2023-06-07 12:44:52 +0530

    Create profile page.

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from . models import Profile
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email', 'address', 'github', 'qualification', 'tech_skills', 'designation', 'about', 'interests', 'image')
+
 # Register your models here.
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Profile
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index page.")
+    profiles = Profile.objects.all()
+    return render(request, 'index.html', {'profiles': profiles})
